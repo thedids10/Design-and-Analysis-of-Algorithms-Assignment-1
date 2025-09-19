863006fe9620c3699138f6a41e097b2f72a2474e
Nursultan Tursunbaev <72702088+thedids10@users.noreply.github.com>
2025-09-19 19:33:17 +0500

    QuickSort.java version 2.0

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,36 +2,34 @@ import java.util.Random;
 
 public class QuickSort {
 
-    // Главный метод для сортировки массива
-    public static void quickSort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
+    private static final Random rand = new Random();
+
+    public static void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
     }
 
-    // Вспомогательный метод для рекурсивной сортировки
     private static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            // Разделение массива и получение индекса опорного элемента
-            int pivotIndex = partition(arr, low, high);
+        while (low < high) {
+            int pivotIndex = low + rand.nextInt(high - low + 1);
+            swap(arr, pivotIndex, high);
 
-            // Рекурсивные вызовы для левой и правой части массива
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
+            int p = partition(arr, low, high);
+
+        
+            if (p - low < high - p) {
+                quickSort(arr, low, p - 1);
+                low = p + 1;
+            } else {
+                quickSort(arr, p + 1, high);
+                high = p - 1;
+            }
         }
     }
 
-    // Метод для разбиения массива с случайным выбором опорного элемента
     private static int partition(int[] arr, int low, int high) {
-        // Случайный выбор опорного элемента
-        Random random = new Random();
-        int pivotIndex = low + random.nextInt(high - low + 1);
-
-        // Перемещаем опорный элемент в конец массива
-        swap(arr, pivotIndex, high);
-
         int pivot = arr[high];
         int i = low - 1;
 
-        // Проходим по массиву и перемещаем элементы меньше опорного влево
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
                 i++;
@@ -39,27 +37,24 @@ public class QuickSort {
             }
         }
 
-        // Перемещаем опорный элемент в его правильное место
         swap(arr, i + 1, high);
-        return i + 1; // Возвращаем индекс опорного элемента
+        return i + 1;
     }
 
-    // Метод для обмена элементов
     private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        if (i != j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
     }
 
     // Пример использования
     public static void main(String[] args) {
-        int[] arr = {10, 7, 8, 9, 1, 5};
-        quickSort(arr);
-
-        // Вывод отсортированного массива
-        System.out.println("Отсортированный массив:");
-        for (int i : arr) {
-            System.out.print(i + " ");
+        int[] array = {9, 3, 7, 1, 5, 4, 6, 8, 2, 0, 11, -3, 12};
+        sort(array);
+        for (int num : array) {
+            System.out.print(num + " ");
         }
     }
 }
